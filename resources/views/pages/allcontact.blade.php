56b5c80568ce5e6f30fa806807b8ab7913427363
AfrinaDipti <diptiafrina24@gmail.com>
2024-09-23 09:33:53 +0600

    CRUD application in Laravel

---
+++ resources/views/pages/allcontact.blade.php
@@ -0,0 +1,32 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+<h1>Contact Book</h1>
+<a href="{{ URL::to('/') }}" class="btn btn-success">Home</a><br>
+<a href="{{ URL::to('/insert-data') }}" class="btn btn-primary">Add New</a>
+
+<table class="table table-border">
+    <tr>
+       <th>Id</th>
+       <th>Name</th>
+       <th>Email</th>
+       <th>Phone</th>
+       <th>Action</th>
+    </tr>
+    @foreach($datadekhao as $row)
+    <tr>
+    	<td>{{ $row->id }}</td>
+    	<td>{{ $row->name }}</td>
+    	<td>{{ $row->email }}</td>
+    	<td>{{ $row->phone }}</td>
+    	<td>
+    		<a href="{{ URL::to('edit-contact/'.$row->id) }}" class="btn btn-info">Edit</a>
+    		<a href="{{ URL::to('delete-contact/'.$row->id) }}" class="btn btn-danger">Delete</a>
+    		<a href="{{ URL::to('view-contact/'.$row->id) }}" class="btn btn-primary">View</a>
+      </td>
+   </tr>
+   @endforeach
+</table>
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
